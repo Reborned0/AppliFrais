aabc6faa72fd5e8071b3a0b9b875e5978b6ee7a8
Clément NICOLE <cnicole@sio.chaptalqper>
2018-11-08 13:42:27 +0100

    Modif

--- application/javascript/JavaScript.js
+++ application/javascript/JavaScript.js
@@ -1,21 +1,7 @@
-function PasAlpha()
+function Numérique()
 {
   alert("test");
 
-  alert(genCharArray('a','z','A','Z'));
-  var Alpha = genCharArray('a','z','A','Z');
-}
-
-function genCharArray(charA,charZ,chara,charz) {
-  var a = [], i = charA.charCodeAt(0), j = charZ.charCodeAt(0), k = chara.charCodeAt(0), l = charz.charCodeAt(0);
-  for (; i <= j; ++i) {
-    a.push(String.fromCharCode(i));
-  }
-  for(;k <= l; k++){
-    a.push(String.fromCharCode(k));
-  }
-
-  return a;
 }
 
 function total(param)

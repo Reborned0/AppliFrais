4e1ea1fba9ef647c7a96583b83b8675b5e803700
Johan HASCOET <jhascoet@sio.chaptalqper>
2018-11-08 14:13:20 +0100

    chemin java

--- application/javascript/JavaScript.js
+++ application/javascript/JavaScript.js
@@ -6,7 +6,4 @@ function Numérique()
 function total(param)
 {
   alert("chiffre");
-  document.getElementByValue('param');
-  var test += param;
-  return test;
 }
--- application/views/v_visModListeFrais.php
+++ application/views/v_visModListeFrais.php
@@ -21,7 +21,7 @@
 						echo
 						'<p>
 							<label for="'.$idFrais.'">'.$libelle.'</label>
-							<input type="text" onkeypress="NonAlpha()" onchange="total()" id="'.$idFrais.'" name="lesFrais['.$idFrais.']" size="10" maxlength="5" value="'.$quantite.'" />
+							<input type="text" onkeypress="NonAlpha()" onchange="total(this.value)" id="'.$idFrais.'" name="lesFrais['.$idFrais.']" size="10" maxlength="5" value="'.$quantite.'" />
 
 						</p>
 						';

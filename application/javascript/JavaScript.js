9ce5a96a7e3f866de7214326a3fa9551c6b100d1
Clément NICOLE <cnicole@sio.chaptalqper>
2018-11-08 14:15:51 +0100

    Java

--- application/javascript/JavaScript.js
+++ application/javascript/JavaScript.js
@@ -1,6 +1,18 @@
 function maNumérique()
 {
-  alert('test');
+
+}
+
+function genCharArray(charA,charZ,chara,charz) {
+  var a = [], i = charA.charCodeAt(0), j = charZ.charCodeAt(0), k = chara.charCodeAt(0), l = charz.charCodeAt(0);
+  for (; i <= j; ++i) {
+    a.push(String.fromCharCode(i));
+  }
+  for(;k <= l; k++){
+    a.push(String.fromCharCode(k));
+  }
+
+  return a;
 }
 
 function total(param)

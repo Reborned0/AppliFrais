81af647ca3d0950997d01ae4ee9f9fc00980bdbf
Reborned0 <bob.clement@hotmail.fr>
2018-11-20 22:27:48 +0100

    A voir

--- application/views/v_visModListeFrais.php
+++ application/views/v_visModListeFrais.php
@@ -21,35 +21,35 @@ $this->load->helper('url');
 					foreach ($lesFraisForfait as $unFrais)
 					{
 						?><tr>
-						<td><?php
-						$idFrais = $unFrais['idfrais'];
-						$libelle = $unFrais['libelle'];
-						$montantCoutFraisForfait = $unFrais['montantFrais'];
-						$quantite = $unFrais['quantite'];
+							<td><?php
+							$idFrais = $unFrais['idfrais'];
+							$libelle = $unFrais['libelle'];
+							$montantCoutFraisForfait = $unFrais['montantFrais'];
+							$quantite = $unFrais['quantite'];
 
-						echo "<script type='text/javascript'>
-						TableaudesMontants['$idFrais']='$unFrais[montantFrais]';
-						console.log(TableaudesMontants);
-						</script>";
+							echo "<script type='text/javascript'>
+							TableaudesMontants['$idFrais']='$unFrais[montantFrais]';
+							console.log(TableaudesMontants);
+							</script>";
 
 
-						echo
-						'<p>
-						<label for="'.$idFrais.'">'.$libelle.'</label>
-						<input type="text" onkeyup="ChercheAlpha(this);calculForfait();CalculFraisParFrais(this,TableaudesMontants);TotalFraisParFrais();" onkeypress="ChercheAlpha(this);calculForfait();" class="SearchAlpha" id="'.$idFrais.'" name="lesFrais['.$idFrais.']" size="10" maxlength="5" value="'.$quantite.'" />
-						</p>
-						';
-						?></td><td><?php
+							echo
+							'<p>
+							<label for="'.$idFrais.'">'.$libelle.'</label>
+							<input type="text" onkeyup="ChercheAlpha(this);calculForfait();CalculFraisParFrais(this,TableaudesMontants);TotalFraisParFrais();" onkeypress="ChercheAlpha(this);calculForfait();" class="SearchAlpha" id="'.$idFrais.'" name="lesFrais['.$idFrais.']" size="10" maxlength="5" value="'.$quantite.'" />
+							</p>
+							';
+							?></td><td><?php
 
-						$resultCouts = $quantite * $montantCoutFraisForfait;
+							$resultCouts = $quantite * $montantCoutFraisForfait;
 
-						echo '
-						<p>
-						<label>Résultat </label>
-						<input disabled type="text" class="ApplicationCalcul" id="Resul'.$idFrais.'" size="10" maxlength="5" value="">
-						';
-						$i++;
-						?></td>
+							echo '
+							<p>
+							<label>Résultat </label>
+							<input disabled type="text" class="ApplicationCalcul" id="Resul'.$idFrais.'" size="10" maxlength="5" value="">
+							';
+							$i++;
+							?></td>
 						</tr><?php
 
 					}
@@ -60,14 +60,14 @@ $this->load->helper('url');
 					//echo "Résultat : ".$result;
 					//}
 					?><tr>
-					<td><?php
-					echo
-					'<p>
-					<label>Total </label>
-					<input disabled type="text" name="Total" id="Total" size="10" maxlength="5" value="" />
-					</p>
-					';
-					?>
+						<td><?php
+						echo
+						'<p>
+						<label>Total </label>
+						<input disabled type="text" name="Total" id="Total" size="10" maxlength="5" value="" />
+						</p>
+						';
+						?>
 					</td>
 					<td><?php
 					echo
@@ -77,87 +77,93 @@ $this->load->helper('url');
 					</p>
 					';
 					?></td>
-					</tr>
+				</tr>
 
-					</table>
-					</fieldset>
-
-					</div>
-					<div class="piedForm">
-					<p>
-					<input id="ok" type="submit" value="Enregistrer" size="20" />
-					<input id="annuler" type="reset" value="Effacer" size="20" />
-					</p>
-					</div>
-					</form>
-
-
-					<table class="listeLegere">
-					<caption>Descriptif des éléments hors forfait</caption>
-					<tr>
-					<th >Date</th>
-					<th >Libellé</th>
-					<th >Montant</th>
-					<th >&nbsp;</th>
-					</tr>
-
-					<?php
-					foreach( $lesFraisHorsForfait as $unFraisHorsForfait)
-					{
-						$libelle = $unFraisHorsForfait['libelle'];
-						$date = $unFraisHorsForfait['date'];
-						$montant=$unFraisHorsForfait['montant'];
-						$id = $unFraisHorsForfait['id'];
-						echo
-						'<tr>
-						<td class="date">'.$date.'</td>
-						<td class="libelle">'.$libelle.'</td>
-						<td class="montant">'.$montant.'</td>
-						<td class="action">'.
-						anchor(	"c_visiteur/supprFrais/$id",
-						"Supprimer ce frais",
-						'title="Suppression d\'une ligne de frais" onclick="return confirm(\'Voulez-vous vraiment supprimer ce frais ?\');"'
-						).
-						'</td>
-						</tr>';
-					}
-					?>
-
-				</script>
 			</table>
+		</fieldset>
 
-			<?php if (isset($erreur2))	echo '<div class ="erreur2"><ul><li>'.$erreur2.'</li></ul></div>'; ?>
-
-			<form method="post" action="<?php echo base_url("c_visiteur/ajouteFrais");?>">
-				<div class="corpsForm">
-					<fieldset>
-						<legend>Nouvel élément hors forfait</legend>
-						<p>
-							<label for="txtDateHF">Date (jj/mm/aaaa): </label>
-							<input type="date" id="txtDateHF" required name="dateFrais" size="10" maxlength="10" value=""  />
-						</p>
-						<p>
-							<label for="txtLibelleHF">Libellé</label>
-							<input type="text" id="txtLibelleHF" required name="libelle" size="60" maxlength="256" value="" />
-						</p>
-						<p>
-							<label for="txtMontantHF">Montant : </label>
-							<input type="text" id="txtMontantHF" name="montant" size="10" maxlength="10" value="" />
-						</p>
-					</fieldset>
-				</div>
-				<div class="piedForm">
-					<p>
-						<label for="txtMontantHF">Montant : </label>
-						<input type="text" id="txtMontantHF" required name="montant" size="10" maxlength="10" value="" />
-					</p>
-				</fieldset>
-			</div>
-			<div class="piedForm">
-				<p>
-					<input id="ajouter" type="submit" value="Ajouter" size="20" />
-					<input id="effacer" type="reset" value="Effacer" size="20" />
-				</p>
-			</div>
-		</form>
 	</div>
+	<div class="piedForm">
+		<p>
+			<input id="ok" type="submit" value="Enregistrer" size="20" />
+			<input id="annuler" type="reset" value="Effacer" size="20" />
+		</p>
+	</div>
+</form>
+
+
+<table class="listeLegere">
+	<caption>Descriptif des éléments hors forfait</caption>
+	<tr>
+		<th >Date</th>
+		<th >Libellé</th>
+		<th >Montant</th>
+		<th >&nbsp;</th>
+	</tr>
+
+	<?php
+	$total=0;
+	foreach( $lesFraisHorsForfait as $unFraisHorsForfait)
+	{
+		$libelle = $unFraisHorsForfait['libelle'];
+		$date = $unFraisHorsForfait['date'];
+		$montant=$unFraisHorsForfait['montant'];
+		$id = $unFraisHorsForfait['id'];
+		echo
+		'<tr>
+		<td class="date">'.$date.'</td>
+		<td class="libelle">'.$libelle.'</td>
+		<td class="montant">'.$montant.'</td>
+		<td class="action">'.
+		anchor(	"c_visiteur/supprFrais/$id",
+		"Supprimer ce frais",
+		'title="Suppression d\'une ligne de frais" onclick="return confirm(\'Voulez-vous vraiment supprimer ce frais ?\');"'
+		).
+		'</td>
+		</tr>';
+		$total+=$montant;
+	}
+	?>
+	<tr>
+		<td class="date"></td>
+		<td class="libelle"><b>Montant Total</b></td>
+		<td class="montant"><b><?= $total ?></b></td>
+	</tr>
+</script>
+</table>
+
+<?php if (isset($erreur2))	echo '<div class ="erreur2"><ul><li>'.$erreur2.'</li></ul></div>'; ?>
+
+<form method="post" action="<?php echo base_url("c_visiteur/ajouteFrais");?>">
+	<div class="corpsForm">
+		<fieldset>
+			<legend>Nouvel élément hors forfait</legend>
+			<p>
+				<label for="txtDateHF">Date (jj/mm/aaaa): </label>
+				<input type="date" id="txtDateHF" required name="dateFrais" size="10" maxlength="10" value=""  />
+			</p>
+			<p>
+				<label for="txtLibelleHF">Libellé</label>
+				<input type="text" id="txtLibelleHF" required name="libelle" size="60" maxlength="256" value="" />
+			</p>
+			<p>
+				<label for="txtMontantHF">Montant : </label>
+				<input type="text" id="txtMontantHF" name="montant" size="10" maxlength="10" value="" />
+			</p>
+		</fieldset>
+	</div>
+	<div class="piedForm">
+		<p>
+			<label for="txtMontantHF"> A VOIIIIIRRRRRRRRRRRRR : </label>
+			<input type="text" id="txtMontantHF" required name="montant" size="10" maxlength="10" value="" />
+		</p>
+	</fieldset>
+</div>
+<div class="piedForm">
+	<p>
+		<input id="ajouter" type="submit" value="Ajouter" size="20" />
+		<input id="effacer" type="reset" value="Effacer" size="20" />
+	</p>
+</div>
+</form>
+</div>

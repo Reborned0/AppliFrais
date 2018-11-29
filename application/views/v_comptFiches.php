16aff1a4e03144cc69916ae21564aee54e710fca
JohanH <custum.johan@hotmail.fr>
2018-11-29 17:05:34 +0100

    fdghdghdf

--- application/views/v_comptFiches.php
+++ application/views/v_comptFiches.php
@@ -14,6 +14,7 @@
 				<th >Etat</th>
 				<th >Montant</th>
 				<th >Date modif.</th>
+        <th >Actions</th>
 			</tr>
 		</thead>
 		<tbody>
@@ -26,10 +27,11 @@
 				echo//MODIFIER ICI
 				'<tr>
           <td class="libelle">'.$uneFiche['idVisiteur'].'</td>
-					<td class="date">'.anchor('c_visiteur/voirFiche/'.$uneFiche['mois'], $uneFiche['mois'],  'title="Consulter la fiche"').'</td>
+					<td class="date">'.$uneFiche['mois'].'</td>
 					<td class="libelle">'.$uneFiche['libelle'].'</td>
 					<td class="montant">'.$uneFiche['montantValide'].'</td>
 					<td class="date">'.$uneFiche['dateModif'].'</td>
+          <td class="date">'.anchor('c_comptable/voirFiche/'.$uneFiche['mois'], 'Consulter',  'title="Consulter la fiche"').'</td>
 				</tr>';
       }
 			}
